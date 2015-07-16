b7ae319c1e4df0c7ec72dabbbeb80ccdfe85d436
Jørgen Sivesind <jsi@enonic.com>
2015-07-16 15:03:35 +0200

    XP-582 : Improve Google Maps part by adding Facet of cities by distance from selected city. - New facet created.  Some test data improved.

---
+++ src/main/resources/app/parts/cities-distance-facet/cities-distance-facet.js
@@ -0,0 +1,103 @@
+var portal = require('/lib/xp/portal');
+var thymeleaf = require('/lib/xp/thymeleaf');
+var contentSvc = require('/lib/xp/content');
+
+var view = resolve('cities-distance-facet.part.html');
+var service = require('service.js').service;
+
+function handleGet(req) {
+
+    var currentCityName;
+    var cities;
+
+    if (req.params.city) {
+        var city = getCity(req.params.city);
+        if (city) {
+            currentCityName = city.displayName;
+            var cityLocation = city.data.cityLocation;
+            cityLocation = cityLocation || "NaN,NaN";
+            var coordinates = cityLocation.split(",");
+            cities = contentSvc.query({
+                    start: 0,
+                    count: 50,
+                    contentTypes: [
+                        module.name + ':city'
+                    ],
+                    "sort": "geoDistance('data.cityLocation','" + city.data.cityLocation + "')",
+                    "query": "_name != '" + currentCityName + "'",
+                    "aggregations": {
+                        "distance": {
+                            "geo_distance": {
+                                'field': "data.cityLocation",
+                                'unit': "km",
+                                'origin': {
+                                    'lat': coordinates[0],
+                                    'lon': coordinates[1]
+                                },
+                                'ranges': [ { 'from': 0, 'to': 1200 }, { 'from': 1200, 'to': 4000 }, { 'from': 4000, 'to': 12000 }, {'from': 12000} ]
+                            }
+                        }
+                    }
+                }
+            );
+        }
+    }
+
+    if (!currentCityName) {
+        currentCityName = "None";
+    }
+
+    if (!cities) {
+        cities = contentSvc.query({
+                start: 0,
+                count: 25,
+                contentTypes: [
+                    module.name + ':city'
+                ]
+            }
+        );
+    }
+
+    log.info(cities.constructor === Array);
+    log.info(Array.isArray(cities));
+
+    var content = portal.getContent();
+    var currentPage = portal.pageUrl({
+        path: content._path
+    });
+
+    var buckets;
+    if (cities.aggregations.distance) {
+        buckets = cities.aggregations.distance.buckets;
+    }
+
+    var params = {
+        cities: cities.contents,
+        total: cities.total,
+        buckets: buckets,
+        currentCity: currentCityName,
+        currentPage: currentPage
+    };
+
+    var body = thymeleaf.render(view, params);
+
+    return {
+        contentType: 'text/html',
+        body: body
+    };
+
+    function getCity(cityName) {
+        var result = contentSvc.query({
+                count: 1,
+                contentTypes: [
+                    module.name + ':city'
+                ],
+                "query": "_name = '" + cityName + "'"
+            }
+        );
+
+        return result.contents[0];
+    }
+}
+
+exports.get = handleGet;
