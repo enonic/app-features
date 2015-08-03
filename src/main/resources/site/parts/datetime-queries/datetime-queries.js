bb58b2fc6ebaa95d0e17bbdec0a953fe699b9ecb
Runar Myklebust <rmy@enonic.com>
2015-08-03 16:17:50 +0200

    XP-1147 Create test-page for datetime-queries

---
+++ src/main/resources/site/parts/datetime-queries/datetime-queries.js
@@ -0,0 +1,65 @@
+var portal = require('/lib/xp/portal');
+var thymeleaf = require('/lib/xp/thymeleaf');
+var contentSvc = require('/lib/xp/content');
+
+var view = resolve('datetime-queries.page.html');
+
+function handleGet(req) {
+
+    var d = new Date();
+    var nowISO = d.toISOString();
+    var now = nowISO.slice(0, -1);
+
+    log.info(now);
+
+    var futureWithTZ = contentSvc.query({
+        start: 0,
+        count: 25,
+        sort: 'data.datetime DESC',
+        query: "_parentPath = '/content/features/input-types/date-and-time/datetime-queries' AND data.requiredDatetime > dateTime('" +
+               nowISO + "')"
+    });
+
+    var pastWithTZ = contentSvc.query({
+        start: 0,
+        count: 25,
+        sort: 'data.datetime DESC',
+        query: "_parentPath = '/content/features/input-types/date-and-time/datetime-queries' AND data.requiredDatetime < dateTime('" +
+               nowISO + "')"
+    });
+
+    var futureNoTZ = contentSvc.query({
+        start: 0,
+        count: 25,
+        sort: 'data.datetime DESC',
+        query: "_parentPath = '/content/features/input-types/date-and-time/datetime-queries' AND data.datetime > '" + now + "'"
+    });
+
+    var pastNoTZ = contentSvc.query({
+        start: 0,
+        count: 25,
+        sort: 'data.datetime DESC',
+        query: "_parentPath = '/content/features/input-types/date-and-time/datetime-queries' AND data.datetime < '" + now + "'"
+    });
+
+    var content = portal.getContent();
+    var currentPage = portal.pageUrl({
+        path: content._path
+    });
+
+    var params = {
+        futureWithTZ: futureWithTZ.contents,
+        futureNoTZ: futureNoTZ.contents,
+        pastWithTZ: pastWithTZ.contents,
+        pastNoTZ: pastNoTZ.contents,
+        currentPage: currentPage
+    };
+    var body = thymeleaf.render(view, params);
+
+    return {
+        contentType: 'text/html',
+        body: body
+    };
+}
+
+exports.get = handleGet;
