d716cd5b6c6cfa44b9279b2dd7a539c80d423319
Glenn Ricaud <gri@enonic.com>
2015-10-05 12:47:02 +0200

    XP-1758 API and Reference Guide - Javascript libraries

---
+++ src/main/resources/site/lib/jslibraries/content.js
@@ -0,0 +1,60 @@
+exports.create = function () {
+
+    //Documentation BEGIN
+    var contentLib = require('/lib/xp/content');
+
+    var result = contentLib.create({
+        name: 'mycontent',
+        parentPath: '/features/js-libraries',
+        displayName: 'My Content',
+        contentType: app.name + ':all-input-types',
+        language: 'no',
+        data: {
+            myCheckbox: true,
+            myComboBox: "option1",
+            myDate: "1970-01-01",
+            myDateTime: "1970-01-01T10:00",
+            myDouble: 1.2,
+            myGeoPoint: "59.91,10.75",
+            myHtmlArea: "<p>htmlAreaContent</p>",
+            myImageSelector: "5a5fc786-a4e6-4a4d-a21a-19ac6fd4784b",
+            myLong: 123,
+            myRelationship: "features",
+            myRadioButtons: "option1",
+            myTag: "aTag",
+            myTextArea: "textAreaContent",
+            myTextLine: "textLineContent",
+            myTime: "10:00",
+            myTextAreas: [
+                "textAreaContent1",
+                "textAreaContent2"
+            ],
+            myItemSet: {
+                "textLine": "textLineContent",
+                "long": 123
+            }
+        }
+    });
+
+    log.info('Content created with id ' + result._id);
+    //Documentation END
+
+    log.info('Create result: ' + JSON.stringify(result, null, 4));
+
+    return result;
+};
+
+exports.get = function () {
+
+    //Documentation BEGIN
+    var contentLib = require('/lib/xp/content');
+
+    var result = contentLib.get({
+        key: '/features/js-libraries/mycontent'
+    });
+    //Documentation END
+
+    log.info('Get result: ' + JSON.stringify(result, null, 4));
+
+    return result;
+};
---
+++ src/main/resources/site/parts/jslibraries/jslibraries.js
@@ -0,0 +1,24 @@
+var portal = require('/lib/xp/portal');
+var thymeleaf = require('/lib/xp/thymeleaf');
+var contentJsLib = require('/lib/jslibraries/content');
+var view = resolve('jslibraries.html');
+
+function handleGet(req) {
+
+    var createResult = JSON.stringify(contentJsLib.create(), null, 4);
+    var getResult = JSON.stringify(contentJsLib.get(), null, 4);
+
+    var params = {
+        createResult: createResult,
+        getResult: getResult
+    };
+
+    var body = thymeleaf.render(view, params);
+
+    return {
+        contentType: 'text/html',
+        body: body
+    };
+}
+
+exports.get = handleGet;
