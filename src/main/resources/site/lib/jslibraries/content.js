b1b15ae2c6e5fa817d63716eb8246b4067216a6b
Glenn Ricaud <gri@enonic.com>
2015-10-05 13:20:08 +0200

    XP-1758 API and Reference Guide - Javascript libraries

--- src/main/resources/site/lib/jslibraries/content.js
+++ src/main/resources/site/lib/jslibraries/content.js
@@ -7,31 +7,40 @@ exports.create = function () {
         name: 'mycontent',
         parentPath: '/features/js-libraries',
         displayName: 'My Content',
+        requireValid: true,
         contentType: app.name + ':all-input-types',
+        branch: 'draft',
         language: 'no',
         data: {
             myCheckbox: true,
-            myComboBox: "option1",
-            myDate: "1970-01-01",
-            myDateTime: "1970-01-01T10:00",
-            myDouble: 1.2,
-            myGeoPoint: "59.91,10.75",
-            myHtmlArea: "<p>htmlAreaContent</p>",
-            myImageSelector: "5a5fc786-a4e6-4a4d-a21a-19ac6fd4784b",
+            myComboBox: 'option1',
+            myDate: '1970-01-01',
+            myDateTime: '1970-01-01T10:00',
+            myDouble: 3.14,
+            myGeoPoint: '59.91,10.75',
+            myHtmlArea: '<p>htmlAreaContent</p>',
+            myImageSelector: '5a5fc786-a4e6-4a4d-a21a-19ac6fd4784b',
             myLong: 123,
-            myRelationship: "features",
-            myRadioButtons: "option1",
-            myTag: "aTag",
-            myTextArea: "textAreaContent",
-            myTextLine: "textLineContent",
-            myTime: "10:00",
+            myRelationship: 'features',
+            myRadioButtons: 'option1',
+            myTag: 'aTag',
+            myTextArea: 'textAreaContent',
+            myTextLine: 'textLineContent',
+            myTime: '10:00',
             myTextAreas: [
-                "textAreaContent1",
-                "textAreaContent2"
+                'textAreaContent1',
+                'textAreaContent2'
             ],
             myItemSet: {
-                "textLine": "textLineContent",
-                "long": 123
+                'textLine': 'textLineContent',
+                'long': 123
+            }
+        },
+        x: {
+            "com-enonic-app-features": {
+                "menu-item": {
+                    "menuItem": true
+                }
             }
         }
     });
