814b54997e1ca86d0a5c4703dbb88939f05d851c
Alex Rodriguez <aro@enonic.com>
2015-10-30 13:05:13 +0100

    XP-1803 Content API - Automatically create content name on server Added part for testing content.create()

---
+++ src/main/resources/site/assets/js/parts/createContent/createContent.js
@@ -0,0 +1,26 @@
+$(function () {
+
+    $(document).on('click', 'form button[name="create"]', function (e) {
+        e.preventDefault();
+        var form = $(this).closest('form');
+        postForm(form);
+    });
+
+});
+
+function postForm(form) {
+    $.ajax({
+        url: form.attr('action'),
+        method: "POST",
+        contentType: 'application/x-www-form-urlencoded; charset=UTF-8',
+        data: {
+            name: form.find('input[name="contentName"]').val(),
+            displayName: form.find('input[name="displayName"]').val(),
+            parent: form.find('input[name="parentPath"]').val(),
+            contentType: form.find('input[name="contentType"]').val(),
+            contentData: form.find('textarea[name="contentData"]').val()
+        }
+    }).done(function (resp) {
+        form.closest('.create-content-part').replaceWith(resp);
+    });
+}
---
+++ src/main/resources/site/parts/createContent/createContent.js
@@ -0,0 +1,97 @@
+var portal = require('/lib/xp/portal');
+var thymeleaf = require('/lib/xp/thymeleaf');
+var contentSvc = require('/lib/xp/content');
+
+exports.get = function (req) {
+    var component = portal.getComponent();
+    var targetFolder = component.config.targetFolder;
+    var parentPath = '';
+    if (targetFolder) {
+        var folder = contentSvc.get({
+            key: targetFolder
+        });
+        parentPath = folder ? folder._path : '';
+    }
+
+    var postUrl = portal.componentUrl({});
+
+    var params = {
+        postUrl: postUrl,
+        parentPath: parentPath,
+        displayName: '',
+        contentName: '',
+        contentType: 'base:unstructured'
+    };
+
+    var view = resolve('createContent.html');
+    var body = thymeleaf.render(view, params);
+
+    return {
+        contentType: 'text/html',
+        body: body,
+        pageContributions: {
+            bodyEnd: [
+                '<script src="' + portal.assetUrl({path: 'js/jquery-2.1.4.min.js'}) + '" type="text/javascript"></script>',
+                '<script src="' + portal.assetUrl({path: 'js/parts/createContent/createContent.js'}) + '" type="text/javascript"></script>',
+            ]
+        }
+    };
+};
+
+exports.post = function (req) {
+    var name = req.params.name;
+    var displayName = req.params.displayName;
+    var parentPath = req.params.parent;
+    var contentType = req.params.contentType || 'base:unstructured';
+    var dataStr = req.params.contentData || '{}';
+
+    var errorMsg;
+    var msg;
+    try {
+        var data = JSON.parse(dataStr);
+
+        var createResult = contentSvc.create({
+            name: name,
+            parentPath: parentPath,
+            displayName: displayName,
+            contentType: contentType,
+            data: data
+        });
+
+        msg = 'Content created: ' + createResult._path;
+        name = createResult._name;
+        dataStr = JSON.stringify(createResult.data, null, 4);
+    } catch (e) {
+        if (e.code === 'contentAlreadyExist') {
+            errorMsg = 'There is already a content with that name';
+        } else {
+            errorMsg = 'Error: ' + e.message;
+        }
+    }
+
+    var postUrl = portal.componentUrl({});
+    var params = {
+        postUrl: postUrl,
+        parentPath: parentPath,
+        displayName: displayName,
+        contentName: name,
+        contentType: contentType,
+        contentData: dataStr,
+        errorMsg: errorMsg,
+        msg: msg
+    };
+
+    var view = resolve('createContent.html');
+    var body = thymeleaf.render(view, params);
+
+    return {
+        contentType: 'text/html',
+        body: body,
+        pageContributions: {
+            bodyEnd: [
+                '<script src="' + portal.assetUrl({path: 'js/jquery-2.1.4.min.js'}) + '" type="text/javascript"></script>',
+                '<script src="' + portal.assetUrl({path: 'js/parts/createContent/createContent.js'}) + '" type="text/javascript"></script>',
+            ]
+        }
+    };
+};
