8e68d8fbffe6d28ad6f715bed334c1e145104531
Glenn Ricaud <gri@enonic.com>
2015-11-24 16:08:11 +0100

    XP-2220 Security functions - JS doc & XP documentation

---
+++ src/main/resources/site/lib/jslibraries/context.js
@@ -0,0 +1,47 @@
+exports.runWithUser = function () {
+
+    //Documentation BEGIN
+    var authLib = require('/lib/xp/auth');
+    var contextLib = require('/lib/xp/context');
+
+    var result = contextLib.runWith({
+        user: 'su'
+    }, authLib.getUser);
+
+    if (result) {
+        log.info('Current user nickname: ' + result.nickname);
+    } else {
+        log.info('No current user');
+    }
+    //Documentation END
+
+    log.info('RunWithUser result: ' + JSON.stringify(result, null, 4));
+
+    return result;
+};
+
+exports.runWithBranch = function () {
+
+    //Documentation BEGIN
+    var contentLib = require('/lib/xp/content');
+    var contextLib = require('/lib/xp/context');
+
+    function getNumberOfContents() {
+        return {
+            total: contentLib.query({count: 0}).total
+        }
+    }
+
+    var result = contextLib.runWith({
+        branch: 'master'
+    }, getNumberOfContents);
+
+    if (result) {
+        log.info('Number of contents on master: ' + result.total);
+    }
+    //Documentation END
+
+    log.info('RunWithBranch result: ' + JSON.stringify(result, null, 4));
+
+    return result;
+};
