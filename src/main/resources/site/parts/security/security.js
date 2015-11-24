56320988b83305975b8ee2e6e75c430d22c8b7dd
Glenn Ricaud <gri@enonic.com>
2015-11-24 15:00:52 +0100

    XP-2220 Security functions - JS doc & XP documentation

--- src/main/resources/site/parts/security/security.js
+++ src/main/resources/site/parts/security/security.js
@@ -4,11 +4,6 @@ var auth = require('/lib/xp/auth');
 var context = require('/lib/xp/context');
 var view = resolve('security.html');
 
-function a() {
-    return JSON.stringify(auth.getUser(), null, 4);
-}
-
-
 function handleGet(req) {
 
     var getUserResult = auth.getUser();
@@ -28,22 +23,11 @@ function handleGet(req) {
         branch: 'master'
     }, getNumberOfContents);
 
-
-    var updatedContent = content.setPermissions({
-        key: '03c6ae7b-7f48-45f5-973d-1f03606ab928',
-        permissions: [{
-            principal: 'user:system:anonymous',
-            allow: ['READ'],
-            deny: []
-        }]
-    });
-
     var params = {
         getUserResult: getUserResult,
         getUserResultWithContext: getUserResultWithContext,
         getNumberOfContentsResult: getNumberOfContentsResult,
-        getNumberOfContentsOnMasterResult: getNumberOfContentsOnMasterResult,
-        updatedContent: updatedContent
+        getNumberOfContentsOnMasterResult: getNumberOfContentsOnMasterResult
     };
 
     var body = thymeleaf.render(view, params);
