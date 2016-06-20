23b8e2e01d29aa1680ff551a513104903c31a593
Alex Rodriguez <aro@enonic.com>
2016-06-20 09:19:12 +0200

    Fixed null in HtmlArea part

--- src/main/resources/site/parts/htmlarea/htmlarea.js
+++ src/main/resources/site/parts/htmlarea/htmlarea.js
@@ -6,11 +6,9 @@ exports.get = function() {
     var content = portal.getContent();
     var view = resolve('htmlarea.html');
 
-    log.info("content %s", JSON.stringify(content, null, 4));
-
     var params = {
         content: content,
-        htmlareavalue: content.data.htmlarea_text
+        htmlareavalue: content.data.htmlarea_text || ''
     };
 
     var body = thymeleaf.render(view, params);
