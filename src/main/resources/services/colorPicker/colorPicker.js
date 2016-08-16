f6efc853445a1de3a1efe8215e8c3a470c5481f9
Pavel Milkevich <pmi@enonic.com>
2016-08-16 18:07:43 +0300

    XP-3837 Implement Color Picker using CustomSelector control

---
+++ src/main/resources/services/colorPicker/colorPicker.js
@@ -0,0 +1,56 @@
+var contentLib = require('/lib/xp/content');
+var contextLib = require('/lib/xp/context');
+var portalLib = require('/lib/xp/portal');
+
+function handleGet(req) {
+
+    log.info("ColorPicker service get: %s", req.params);
+
+    var body = {
+        total: 16,
+        count: 16,
+        hits: [
+            getItemFor('#FFFFFF', 'White'),
+            getItemFor('#C0C0C0', 'Silver'),
+            getItemFor('#808080', 'Gray'),
+            getItemFor('#000000', 'Black'),
+            getItemFor('#FF0000', 'Red'),
+            getItemFor('#800000', 'Maroon'),
+            getItemFor('#FFFF00', 'Yellow'),
+            getItemFor('#808000', 'Olive'),
+            getItemFor('#00FF00', 'Lime'),
+            getItemFor('#008000', 'Green'),
+            getItemFor('#00FFFF', 'Aqua'),
+            getItemFor('#008080', 'Teal'),
+            getItemFor('#0000FF', 'Blue'),
+            getItemFor('#000080', 'Navy'),
+            getItemFor('#FF00FF', 'Fuchsia'),
+            getItemFor('#800080', 'Purple')
+        ]
+    };
+
+    return {
+        contentType: 'application/json',
+        body: body
+    }
+}
+
+function getItemFor(color, desc) {
+    return {
+        id: color,
+        displayName: color,
+        description: desc,
+        icon: getIconFor(color)
+    }
+}
+
+function getIconFor(color) {
+    return {
+        data: '<svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="32" height="32" viewBox="0 0 32 32">' +
+        '<rect x="0" y="0" width="32" height="32" rx="4" ry="4" fill="' + color + '"/>' +
+        '</svg>',
+        type: 'image/svg+xml'
+    }
+}
+
+exports.get = handleGet;
