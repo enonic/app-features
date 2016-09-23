39932fad428a8ca21019329ebc30d0116a0beb59
Glenn Ricaud <gri@enonic.com>
2016-09-23 10:28:11 +0200

    XP-4126 Expose create repository in repo-lib

--- src/main/resources/site/lib/jslibraries/repo.js
+++ src/main/resources/site/lib/jslibraries/repo.js
@@ -1,26 +1,36 @@
 var repoLib = require('/lib/xp/repo.js');
 
-//TODO Waiting for delete
-//exports.create = function () {
-//    var result = repoLib.create({
-//        id: 'test-repo',
-//        validation: {
-//            checkExists: false,
-//            checkParentExists: false
-//        }
-//    });
-//
-//    log.info('Repository created with id ' + result.id);
-//    return result;
-//};
-//
-//exports.get = function () {
-//    var result = repoLib.get({
-//        id: 'test-repo'
-//    });
-//
-//    return result;
-//};
+exports.create = function () {
+
+    var repo = repoLib.get({
+        id: 'system-repo'
+    });
+
+    if (repo) {
+        log.info('Repository [system-repo] already exists');
+        return 'Repository [system-repo] already exists';
+    } else {
+        var result = repoLib.create({
+            id: 'test-repo',
+            validation: {
+                checkExists: false,
+                checkParentExists: false
+            }
+        });
+
+        log.info('Repository [' + result.id + '] was created');
+        return result;
+    }
+
+};
+
+exports.get = function () {
+    var result = repoLib.get({
+        id: 'test-repo'
+    });
+
+    return result;
+};
 
 exports.get = function () {
     var result = repoLib.get({
--- src/main/resources/site/parts/js-libraries-repo/js-libraries-repo.js
+++ src/main/resources/site/parts/js-libraries-repo/js-libraries-repo.js
@@ -2,14 +2,16 @@ var portal = require('/lib/xp/portal');
 var thymeleaf = require('/lib/xp/thymeleaf');
 var repoJsLib = require('/lib/jslibraries/repo');
 var view = resolve('js-libraries-repo.html');
+var contentLib = require('/lib/xp/content');
+var contextLib = require('/lib/xp/context');
 
 function handleGet(req) {
 
-    //var createResult = JSON.stringify(repoJsLib.create(), null, 4);
+    var createResult = JSON.stringify(repoJsLib.create(), null, 4);
     var getResult = JSON.stringify(repoJsLib.get(), null, 4);
 
     var params = {
-        //createResult: createResult,
+        createResult: createResult,
         getResult: getResult
     };
 
