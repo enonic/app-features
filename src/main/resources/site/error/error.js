618be578588e4b71ac074257a727fa543fd6a0d0
Glenn Ricaud <gri@enonic.com>
2016-09-23 10:28:27 +0200

    XP-4126 Expose create repository in repo-lib

--- src/main/resources/site/error/error.js
+++ src/main/resources/site/error/error.js
@@ -1,36 +1,36 @@
-//var thymeleaf = require('/lib/xp/thymeleaf');
-//
-//var view404 = resolve('404.html');
-//var viewGeneric = resolve('default.html');
-//
-//exports.handle404 = function (err) {
-//    var debugMode = err.request.params.debug === 'true';
-//    if (debugMode && (err.request.mode === 'preview' || err.request.mode === 'edit')) {
-//        return null;
-//    }
-//
-//    var params = {};
-//    var body = thymeleaf.render(view404, params);
-//
-//    return {
-//        contentType: 'text/html',
-//        body: body
-//    }
-//};
-//
-//exports.handleError = function (err) {
-//    var debugMode = err.request.params.debug === 'true';
-//    if (debugMode && (err.request.mode === 'preview' || err.request.mode === 'edit')) {
-//        return null;
-//    }
-//
-//    var params = {
-//        errorCode: err.status
-//    };
-//    var body = thymeleaf.render(viewGeneric, params);
-//
-//    return {
-//        contentType: 'text/html',
-//        body: body
-//    }
-//};
+var thymeleaf = require('/lib/xp/thymeleaf');
+
+var view404 = resolve('404.html');
+var viewGeneric = resolve('default.html');
+
+exports.handle404 = function (err) {
+    var debugMode = err.request.params.debug === 'true';
+    if (debugMode && (err.request.mode === 'preview' || err.request.mode === 'edit')) {
+        return null;
+    }
+
+    var params = {};
+    var body = thymeleaf.render(view404, params);
+
+    return {
+        contentType: 'text/html',
+        body: body
+    }
+};
+
+exports.handleError = function (err) {
+    var debugMode = err.request.params.debug === 'true';
+    if (debugMode && (err.request.mode === 'preview' || err.request.mode === 'edit')) {
+        return null;
+    }
+
+    var params = {
+        errorCode: err.status
+    };
+    var body = thymeleaf.render(viewGeneric, params);
+
+    return {
+        contentType: 'text/html',
+        body: body
+    }
+};
