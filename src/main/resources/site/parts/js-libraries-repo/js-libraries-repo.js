fb83acf3068ff287a61b77638b781eac82c3fc22
Glenn Ricaud <gri@enonic.com>
2016-10-14 16:50:28 +0200

    XP-4269 Repo Service API - Validation Settings - Check permissions

--- src/main/resources/site/parts/js-libraries-repo/js-libraries-repo.js
+++ src/main/resources/site/parts/js-libraries-repo/js-libraries-repo.js
@@ -2,17 +2,18 @@ var portal = require('/lib/xp/portal');
 var thymeleaf = require('/lib/xp/thymeleaf');
 var repoJsLib = require('/lib/jslibraries/repo');
 var view = resolve('js-libraries-repo.html');
-var contentLib = require('/lib/xp/content');
-var contextLib = require('/lib/xp/context');
 
 function handleGet(req) {
-
-    var createResult = JSON.stringify(repoJsLib.create(), null, 4);
-    var getResult = JSON.stringify(repoJsLib.get(), null, 4);
+    var createLightRepoResult = JSON.stringify(repoJsLib.create('features-repo-light', true), null, 4);
+    var createDefaultRepoResult = JSON.stringify(repoJsLib.create('features-repo-default'), null, 4);
+    var getLightRepoResult = JSON.stringify(repoJsLib.get('features-repo-light'), null, 4);
+    var getDefaultRepoResult = JSON.stringify(repoJsLib.get('features-repo-default'), null, 4);
 
     var params = {
-        createResult: createResult,
-        getResult: getResult
+        createLightRepoResult: createLightRepoResult,
+        createDefaultRepoResult: createDefaultRepoResult,
+        getLightRepoResult: getLightRepoResult,
+        getDefaultRepoResult: getDefaultRepoResult
     };
 
     var body = thymeleaf.render(view, params);
