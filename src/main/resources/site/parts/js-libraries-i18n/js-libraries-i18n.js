f86ad3d9bdf05eb656e8556efccdb43a4f901674
Jørgen Sivesind <jsi@enonic.com>
2019-04-12 16:52:42 +0200

    enonic/doc-xp # 1 - Improved usage of i18n library with all functions.

--- src/main/resources/site/parts/js-libraries-i18n/js-libraries-i18n.js
+++ src/main/resources/site/parts/js-libraries-i18n/js-libraries-i18n.js
@@ -5,11 +5,13 @@ var view = resolve('js-libraries-i18n.html');
 
 function handleGet(req) {
 
-    var localizeResult = JSON.stringify(i18nJsLib.localize(), null, 4);
+    // var localizeResult = JSON.stringify(i18nJsLib.localize(), null, 4);
+    //
+    // var params = {
+    //     localizeResult: localizeResult
+    // };
 
-    var params = {
-        localizeResult: localizeResult
-    };
+    var params = i18nJsLib.localize();
 
     var body = thymeleaf.render(view, params);
 
